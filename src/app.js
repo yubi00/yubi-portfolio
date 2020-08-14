e362a9c24b038a0ad0ad55dda5070d2290abc833
yubi00 <ubrajkhadka@gmail.com>
2020-08-14 22:27:20 +1000

    heroku deployment setup

--- src/app.js
+++ src/app.js
@@ -11,5 +11,13 @@ app.use(express.json())
 app.use(projectRouter)
 app.use(adminRouter)
 
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static('../client/build'))
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+        
+    })
+}
+
 module.exports = app
 
