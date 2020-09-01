abafec13c3891ed78487a7e0601f34f97ecbcf95
yubi00 <ubrajkhadka@gmail.com>
2020-09-01 23:35:51 +1000

    few css tweaks

--- client/src/components/PortfolioDashboard.js
+++ client/src/components/PortfolioDashboard.js
@@ -6,32 +6,36 @@ import Header from './Header'
 import FooterPage from './FooterPage'
 
 class PortfolioDashboard extends Component {
-    componentDidMount() {
-        this.props.loadUser()
-    }
+  componentDidMount() {
+    this.props.loadUser()
+  }
 
-    render() {
-        return (
-            <div>
-                <Header/>
-                <div className="aboutme">
-                    <div className="content-container">
-                        <div className="aboutme-content">
-                            <img src="/images/yubi.jpg" alt="" className="aboutme-img" />
-                            <div className="aboutme-left">
-                                <h1>Hi, I'm Yubi. </h1>
-                                <h3>I am a Full Stack Developer</h3>
-                                <a href="#myworks" className="view-info">View my works<span><i class="arrow right"></i></span></a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <ProjectList/>
-                <FooterPage/>
+  render() {
+    return (
+      <div>
+        <Header />
+        <div className="aboutme">
+          <div className="content-container">
+            <div className="aboutme-content">
+              <img src="/images/yubi.jpg" alt="" className="aboutme-img" />
+              <div className="aboutme-left">
+                <h1>Hi, I'm Yubi. </h1>
+                <h3>I am a Full Stack Developer</h3>
+                <a href="#myworks" className="view-info">
+                  View my works
+                  <span>
+                    <i className="arrow right"></i>
+                  </span>
+                </a>
+              </div>
             </div>
-        )
-    }
+          </div>
+        </div>
+        <ProjectList />
+        <FooterPage />
+      </div>
+    )
+  }
 }
-
 
 export default connect(undefined, { loadUser })(PortfolioDashboard)
--- tests/project.test.js
+++ tests/project.test.js
@@ -1,96 +1,96 @@
-jest.setTimeout(30000)
+jest.setTimeout(30000);
 
-const request = require('supertest')
-const app = require('../src/app')
+const request = require("supertest");
+const app = require("../src/app");
 
-const { adminOne, adminTwo, projectOne, projectOneId, projectTwo, projectTwoId, setupDatabase } = require('./fixtures/db')
-const Project = require('../src/models/project')
-const { response } = require('express')
+const {
+  adminOne,
+  adminTwo,
+  projectOneId,
+  projectTwoId,
+  setupDatabase,
+} = require("./fixtures/db");
+const Project = require("../src/models/project");
 
-beforeEach(setupDatabase)
+beforeEach(setupDatabase);
 
-test('should fetch projects from database', async () => {
-    const response = await request(app)
-        .get('/projects').
-        send()
-        .expect(200)
+test("should fetch projects from database", async () => {
+  const response = await request(app).get("/projects").send().expect(200);
 
-    expect(response.body.projects.length).toBe(2)
-})
+  expect(response.body.projects.length).toBe(2);
+});
 
-test('should fetch individual project if found', async () => {
-    const response = await request(app)
-        .get(`/projects/${projectOneId}`)
-        .send()
-        .expect(200)
-})
+test("should fetch individual project if found", async () => {
+  const response = await request(app)
+    .get(`/projects/${projectOneId}`)
+    .send()
+    .expect(200);
+});
 
-test('should not create new project for unauthenticated users',async () => {
-    await request(app)
-        .post('/projects')
-        .send({
-            title: "racing app",
-            description: "2d racing game on c++ and sfml",
-            github: "https://github.com/racinggame",
-            site: "https://amazingrace.com"
-        }).expect(401)
-})
+test("should not create new project for unauthenticated users", async () => {
+  await request(app)
+    .post("/projects")
+    .send({
+      title: "racing app",
+      description: "2d racing game on c++ and sfml",
+      github: "https://github.com/racinggame",
+      site: "https://amazingrace.com",
+    })
+    .expect(401);
+});
 
-test('should create new project for authenticated admins', async() => {
-    const response = await request(app)
-        .post('/projects')
-        .set('Authorization',`Bearer ${adminTwo.tokens[0].token}`)
-        .send({
-            title: "racing app",
-            description: "2d racing game on c++ and sfml",
-            github: "https://github.com/racinggame",
-            site: "https://amazingrace.com"
-        })
-        .expect(201)
+test("should create new project for authenticated admins", async () => {
+  const response = await request(app)
+    .post("/projects")
+    .set("Authorization", `Bearer ${adminTwo.tokens[0].token}`)
+    .send({
+      title: "racing app",
+      description: "2d racing game on c++ and sfml",
+      github: "https://github.com/racinggame",
+      site: "https://amazingrace.com",
+    })
+    .expect(201);
 
-    //assert that the new project was added
-    const project = await Project.findById(response.body._id)
-    expect(project).not.toBeNull()
-})
+  //assert that the new project was added
+  const project = await Project.findById(response.body._id);
+  expect(project).not.toBeNull();
+});
 
-test('should not delete project for unauthenticated users', async () => {
-    await request(app)
-        .delete(`/projects/${projectOneId}`)
-        .send()
-        .expect(401)
-})
+test("should not delete project for unauthenticated users", async () => {
+  await request(app).delete(`/projects/${projectOneId}`).send().expect(401);
+});
 
-test('should delete project for autheticated admins', async () => {
-    const response = await request(app)
-        .delete(`/projects/${projectOneId}`)
-        .set('Authorization', `Bearer ${adminOne.tokens[0].token}`)
-        .send()
-        .expect(200)
-    
-        //assert that the project was removed
-    const project = await Project.findById(response.body._id)
-    expect(project).toBeNull()
-})
+test("should delete project for autheticated admins", async () => {
+  const response = await request(app)
+    .delete(`/projects/${projectOneId}`)
+    .set("Authorization", `Bearer ${adminOne.tokens[0].token}`)
+    .send()
+    .expect(200);
 
-test('should not update project for unauthencated users', async () => {
-    await request(app)
-        .patch('/projects/' +projectTwoId)
-        .send({
-            description: "a new description"
-        })
-        .expect(401)
-})
+  //assert that the project was removed
+  const project = await Project.findById(response.body._id);
+  expect(project).toBeNull();
+});
 
-test('should update project for authenticated admins', async () => {
-    const response = await request(app)
-            .patch(`/projects/${projectTwoId}`)
-            .set('Authorization', `Bearer ${adminTwo.tokens[0].token}`)
-            .send({
-                description: "a new desc"
-            })
-            .expect(200)
+test("should not update project for unauthencated users", async () => {
+  await request(app)
+    .patch("/projects/" + projectTwoId)
+    .send({
+      description: "a new description",
+    })
+    .expect(401);
+});
 
-        //assertions about the updates
-    const project = await Project.findById(response.body._id)
-    expect(project.description).toBe("a new desc")
-})
+test("should update project for authenticated admins", async () => {
+  const response = await request(app)
+    .patch(`/projects/${projectTwoId}`)
+    .set("Authorization", `Bearer ${adminTwo.tokens[0].token}`)
+    .send({
+      description: "a new desc",
+    })
+    .expect(200);
+
+  //assertions about the updates
+  const project = await Project.findById(response.body._id);
+  expect(project.description).toBe("a new desc");
+});
