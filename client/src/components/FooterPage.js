91e496af0429891d67b6667fc820f25916c659ea
yubi00 <ubrajkhadka@gmail.com>
2020-11-25 23:18:25 +1100

    materialize socials icon setup

--- client/src/components/FooterPage.js
+++ client/src/components/FooterPage.js
@@ -1,19 +1,27 @@
-import React from 'react'
+import React from 'react';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
 
 const FooterPage = () => (
-    <div className="footer">
-        <div className="footer__contact">
-            <a 
-                href="https://github.com/yubi00"
-                target="_blank" rel="noopener noreferrer"
-            ><img src="/images/github.png" alt="" /></a>
-            <a 
-                href="https://www.linkedin.com/in/ubrajkhadka/"
-                target="_blank" rel="noopener noreferrer"
-            ><img src="/images/linkedin.png" alt=""/></a>
-        </div>
-        <h3 className="footer-content">Copyright &copy; 2020 Yubi Khadka</h3>
+  <div className='footer'>
+    <div className='footer__socials'>
+      <a
+        href='https://github.com/yubi00'
+        target='_blank'
+        rel='noopener noreferrer'
+      >
+        <GitHubIcon fontSize='large' style={{ color: 'white' }} />
+      </a>
+      <a
+        href='https://www.linkedin.com/in/ubrajkhadka/'
+        target='_blank'
+        rel='noopener noreferrer'
+      >
+        <LinkedInIcon fontSize='large' style={{ color: 'white' }} />
+      </a>
     </div>
-)
+    <h3 className='footer-content'>Copyright &copy; 2020 Yubi Khadka</h3>
+  </div>
+);
 
-export default FooterPage
+export default FooterPage;
