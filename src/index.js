54c4bf866e5a44b45e2949a2a6bce7ff8676340d
yubi00 <ubrajkhadka@gmail.com>
2022-12-31 17:57:13 +1100

    add optional dev port

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-const app = require('./app')
-const port = process.env.PORT
+const app = require('./app');
+const port = process.env.PORT || 8080;
 
 app.listen(port, () => {
-    console.log(`Server started on port ${port}`)
-})
+  console.log(`Server started on port ${port}`);
+});
