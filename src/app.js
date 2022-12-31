e2ad97a43b0b716e25922f1532d286ead7efd2fa
yubi00 <ubrajkhadka@gmail.com>
2022-12-31 20:17:47 +1100

    add env file

--- src/app.js
+++ src/app.js
@@ -1,25 +1,22 @@
-const express = require('express')
-const cors = require('cors')
-const path = require('path')
-require('./db/mongoose')
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+require('dotenv').config();
+require('./db/mongoose');
 
-const projectRouter = require('./routers/projects')
-const adminRouter = require('./routers/admins')
+const projectRouter = require('./routers/projects');
+const adminRouter = require('./routers/admins');
 
-const app = new express()
+const app = new express();
 
-app.use(cors())
-app.use(express.json())
-app.use(projectRouter)
-app.use(adminRouter)
+app.use(cors());
+app.use(express.json());
+app.use(projectRouter);
+app.use(adminRouter);
 
-if(process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '/../client/build' )))
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, '/../client', 'build', 'index.html'))
-        
-    })
-}
+app.use(express.static(path.join(__dirname, '/../client/build')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/../client', 'build', 'index.html'));
+});
 
-module.exports = app
-
+module.exports = app;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 const app = require('./app');
 const port = process.env.PORT || 8080;
 
-console.log({ port });
-
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
