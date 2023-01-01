a9b5736f2671d0c96dddc0777f958a8332d92cce
yubi00 <ubrajkhadka@gmail.com>
2023-01-01 13:54:10 +1100

    add vercel config for deployment

--- app.js
+++ app.js
@@ -14,9 +14,9 @@ app.use(express.json());
 app.use(projectRouter);
 app.use(adminRouter);
 
-app.use(express.static(path.join(__dirname, '/../client/build')));
+app.use(express.static(path.join(__dirname, './client/build')));
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/../client', 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, './client', 'build', 'index.html'));
 });
 
 module.exports = app;
